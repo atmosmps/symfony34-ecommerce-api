4bfe061f2aebec4e3d71f8d172f3dcf4965f2cbb
Atmos Maciel <atmos.mps@gmail.com>
2019-01-26 00:35:21 -0300

    Adicionando definicao de rotas por annotation

--- src/ApiBundle/Controller/ProductController.php
+++ src/ApiBundle/Controller/ProductController.php
@@ -7,11 +7,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * Class ProductController
+ * @package ApiBundle\Controller
+ * @Route("/products")
+ */
 class ProductController extends Controller
 {
-
+    /**
+     * @return Response
+     * @Route("", methods={"GET"}, name="product_index")
+     */
     public function indexAction()
     {
         $productsData = $this->getDoctrine()->getRepository('ApiBundle:Product')->findAll();
@@ -19,12 +27,22 @@ class ProductController extends Controller
         return new Response($products);
     }
 
+    /**
+     * @param Product $product
+     * @return object|Response
+     * @Route("/{id}", methods={"GET"}, name="product_get")
+     */
     public function getAction(Product $product)
     {
         $product = $this->get('jms_serializer')->serialize($product, 'json');
         return new Response($product);
     }
 
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @Route("", methods={"POST"}, name="product_save")
+     */
     public function saveAction(Request $request)
     {
         $data = $request->request->all();
@@ -43,6 +61,11 @@ class ProductController extends Controller
         return new JsonResponse(['msg' => 'Produto inserido com sucesso!'], 200);
     }
 
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @Route("", methods={"PUT"}, name="product_update")
+     */
     public function updateAction(Request $request)
     {
         $data = $request->request->all();
@@ -63,6 +86,11 @@ class ProductController extends Controller
         return new JsonResponse(['msg' => 'Produto atualizado com sucesso!'], 200);
     }
 
+    /**
+     * @param Product $product
+     * @return JsonResponse
+     * @Route("/{id}", methods={"DELETE"}, name="product_delete")
+     */
     public function deleteAction(Product $product)
     {
         $doctrine = $this->getDoctrine()->getManager();
