ac1396908b5874530b94136377133ae8818ff4e3
Atmos Maciel <atmos.mps@gmail.com>
2019-03-06 17:13:22 -0300

    Adicionando funcao para checkout do usuario.

--- src/ApiBundle/Controller/CheckoutController.php
+++ src/ApiBundle/Controller/CheckoutController.php
@@ -2,14 +2,36 @@
 
 namespace ApiBundle\Controller;
 
+use ApiBundle\Entity\UserOrder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class CheckoutController
  * @package ApiBundle\Controller
- * @Route("/", name="checkout")
+ * @Route("checkout", name="checkout")
  */
 class CheckoutController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @Route("", methods={"POST"}, name="checkout_index")
+     */
+    public function indexAction(Request $request)
+    {
+        $data = $request->request->all();
+
+        $userOrder = new UserOrder();
+        $userOrder->setItems(serialize($data['items']));
+        $userOrder->setUser($this->getUser());
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($userOrder);
+        $manager->flush();
+
+        return new JsonResponse(['msg' => $userOrder->getId()], 200);
+    }
 }
