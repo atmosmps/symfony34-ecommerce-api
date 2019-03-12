83dd47ca85bd7f483113c454745f6e7bb9dda48f
Atmos Maciel <atmos.mps@gmail.com>
2019-03-12 19:31:53 -0300

    Adicionando configurações iniciais para pagseguro.

--- src/ApiBundle/ApiBundle.php
+++ src/ApiBundle/ApiBundle.php
@@ -6,4 +6,15 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ApiBundle extends Bundle
 {
+    // Sobrescrevendo a function Boot que é executada na inicializacao do Bundle.
+    public function boot()
+    {
+        putenv('PAGSEGURO_ENV=sandbox');
+        putenv('PAGSEGURO_EMAIL=atmos.mps@gmail.com');
+        putenv('PAGSEGURO_TOKEN_SANDBOX=19717EBDD4D54B8A80969F35FDD7C9B6');
+
+        \PagSeguro\Library::initialize();
+        \PagSeguro\Library::cmsVersion()->setName("Symfony34EcommerceApi")->setRelease("1.0.0");
+        \PagSeguro\Library::moduleVersion()->setName("EcommerceApi")->setRelease("1.0.0");
+    }
 }
